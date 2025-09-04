c4c716f12b4de5acad4f4cb79ce4628c3e90b444
bieljose2012 <andressamontei@gmail.com>
2025-09-04 15:29:15 -0300

    Update miamiA16Fork.sh

--- miamiA16Fork.sh
+++ miamiA16Fork.sh
@@ -10,4 +10,5 @@ git clone https://github.com/bieljose2012/proprietary_vendor_motorola_miami.git 
 
 git clone https://github.com/bieljose2012/proprietary_vendor_motorola_sm6375-common.git -b 16.0 vendor/motorola/sm6375-common
 
- https://github.com/bieljose2012/android_hardware_motorola.git-b 16.0 hardware/motorola
+git clone https://github.com/bieljose2012/android_hardware_motorola.git -b 16.0 hardware/motorola
+
