43000e6420373962591f7fc8e19f570cc3d793d7
bieljose2012 <andressamontei@gmail.com>
2025-09-04 15:09:11 -0300

    Add files via upload

---
+++ miamiA16Fork.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/LineageOS/android_device_motorola_miami.git -b lineage-23.0 device/motorola/miami
+
+git clone https://github.com/LineageOS/android_device_motorola_sm6375-common.git -b lineage-23.0 device/motorola/sm6375-common
+
+git clone https://github.com/LineageOS/android_kernel_motorola_sm6375.git -b lineage-23.0 kernel/motorola/sm6375
+
+git clone https://github.com/bieljose2012/proprietary_vendor_motorola_miami.git -b 16.0 vendor/motorola/miami
+
+git clone https://github.com/bieljose2012/proprietary_vendor_motorola_sm6375-common.git -b 16.0 vendor/motorola/sm6375-common
